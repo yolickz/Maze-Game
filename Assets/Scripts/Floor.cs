4ce0067e98b3f1105efe4f8cb2d36fb65b15b1cb
SUSSEXDOWNS\468180 <468180@chrome.sussexdowns.ac.uk>
2018-11-29 14:37:59 +0000

    fully working grid need to make floor spawn over the the maze layout

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+
+    public GameObject floor;
+    private Vector3 floorSpawn;
+    public float sizeX;
+    public float sizeZ;
+
+    // Use this for initialization
+    void Start()
+    {
+        floorSpawn = new Vector3(0.0f, 0.0f, -0.5f);
+        floor.transform.localScale = new Vector3(sizeX, 0.0F, sizeZ);
+        
+        Instantiate(floor, floorSpawn, Quaternion.identity);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -5,34 +5,57 @@ using UnityEngine;
 public class Maze : MonoBehaviour
 {
     public GameObject wall;
+    public GameObject floor;
     public float wallLength = 1.0f;
-    public float xSize = 5.0f;
-    public float ySize = 5.0f;
+    public float xSize;
+    public float ySize;
     private Vector3 initialPos;
-    
+    private Vector3 floorSpawn;
+
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         createWalls();
-	}
+        createFloor();
+    }
 
     void createWalls()
     {
-        initialPos = new Vector3((-xSize / 2) + wallLength/2, 0.0f, (-ySize/2)+ wallLength/2);
+        initialPos = new Vector3((-xSize / 2) + wallLength / 2, 0.0f, (-ySize / 2) + wallLength / 2);
         Vector3 MyPos = initialPos;
+        GameObject tempWall;
+
+        //For vertical walls
         for (int i = 0; i < ySize; i++)
         {
-            for(int j = 0; j<= xSize; j++)
+            for (int j = 0; j <= xSize; j++)
             {
-                MyPos = new Vector3(initialPos.x + (j * wallLength) - wallLength / 2, 0.0f, initialPos.z + (j * wallLength) - wallLength / 2);
+                MyPos = new Vector3(initialPos.x + (j * wallLength) - wallLength / 2, 1f, initialPos.z + (i * wallLength) - wallLength / 2);
                 Instantiate(wall, MyPos, Quaternion.identity);
             }
         }
+        //For horizontal walls
+        for (int i = 0; i <= ySize; i++)
+        {
+            for (int j = 0; j < xSize; j++)
+            {
+                MyPos = new Vector3(initialPos.x + (j * wallLength), 1f, initialPos.z + (i * wallLength) - wallLength);
+                tempWall = Instantiate(wall, MyPos, Quaternion.Euler(0.0f, 90.0f, 0.0f)) as GameObject;
+            }
+        }
     }
-	
-	// Update is called once per frame
-	void Update ()
+    void createFloor()
     {
-		
-	}
+
+        floorSpawn = new Vector3(0.0f, 1f, -0.5f);
+        floor.transform.localScale = new Vector3(wallLength*(xSize/10), 1.0f,wallLength*(ySize / 10));
+
+        Instantiate(floor, floorSpawn, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
