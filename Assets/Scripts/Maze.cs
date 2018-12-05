434134bb9b09a68abd03f210cf07270b879e9e4a
SUSSEXDOWNS\468180 <468180@chrome.sussexdowns.ac.uk>
2018-12-05 14:38:09 +0000

    Almost got wall deleting working

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class Maze : MonoBehaviour
 {
+    [System.Serializable]
+    public class Cell
+    {
+        public bool visited;
+        public GameObject north;
+        public GameObject east;
+        public GameObject south;
+        public GameObject west;
+    }
     public GameObject wall;
     public GameObject floor;
     public float wallLength = 1.0f;
-    public float xSize;
-    public float ySize;
+    public int xSize;
+    public int ySize;
     private Vector3 initialPos;
     private Vector3 floorSpawn;
-
+    private GameObject wallHolder;
+    public Cell[] cells;
     // Use this for initialization
     void Start()
     {
-        createWalls();
-        createFloor();
+        CreateWalls();
+        CreateFloor();
     }
 
-    void createWalls()
+    void CreateWalls()
     {
+        wallHolder = new GameObject();
+        wallHolder.name = "Maze";
+
         initialPos = new Vector3((-xSize / 2) + wallLength / 2, 0.0f, (-ySize / 2) + wallLength / 2);
         Vector3 MyPos = initialPos;
         GameObject tempWall;
@@ -31,7 +44,8 @@ public class Maze : MonoBehaviour
             for (int j = 0; j <= xSize; j++)
             {
                 MyPos = new Vector3(initialPos.x + (j * wallLength) - wallLength / 2, 1f, initialPos.z + (i * wallLength) - wallLength / 2);
-                Instantiate(wall, MyPos, Quaternion.identity);
+                tempWall = Instantiate(wall, MyPos, Quaternion.identity) as GameObject;
+                tempWall.transform.parent = wallHolder.transform;
             }
         }
         //For horizontal walls
@@ -41,14 +55,54 @@ public class Maze : MonoBehaviour
             {
                 MyPos = new Vector3(initialPos.x + (j * wallLength), 1f, initialPos.z + (i * wallLength) - wallLength);
                 tempWall = Instantiate(wall, MyPos, Quaternion.Euler(0.0f, 90.0f, 0.0f)) as GameObject;
+                tempWall.transform.parent = wallHolder.transform;
             }
         }
+        CreateCells();
     }
-    void createFloor()
+    void CreateCells()
+    {
+        GameObject[] allWalls;
+        int children = wallHolder.transform.childCount;
+        allWalls = new GameObject[children];
+        cells = new Cell[xSize * ySize];
+        int eastWestProcess = 0;
+        int childProcess = 0;
+        int termCount = 0;
+
+
+        for (int i = 0; i < children; i++)
+        {
+            allWalls[i] = wallHolder.transform.GetChild(i).gameObject;
+        }
+        for (int cellprocess = 0; cellprocess < cells.Length; cellprocess++)
+        {
+            cells[cellprocess] = new Cell();
+            cells[cellprocess].east = allWalls[eastWestProcess];
+            cells[cellprocess].south = allWalls[children + (xSize + 1) * ySize];
+            if(termCount == xSize)
+            {
+                eastWestProcess += 2;
+                termCount = 0;
+            }
+            else
+            {
+                eastWestProcess ++;
+            }
+            termCount++;
+            childProcess++;
+            cells[cellprocess].west = allWalls[eastWestProcess];
+            cells[cellprocess].south = allWalls[(childProcess + (xSize + 1) * ySize)+xSize+1];
+        }
+
+
+    }
+
+    void CreateFloor()
     {
 
-        floorSpawn = new Vector3(0.0f,1.0f,0.0f);
-        floor.transform.localScale = new Vector3(5.0f,1.0f,5.0f);
+        floorSpawn = new Vector3(0.0f, 1.0f, 0.0f);
+        floor.transform.localScale = new Vector3(5.0f, 1.0f, 5.0f);
 
         Instantiate(floor, floorSpawn, Quaternion.identity);
     }
